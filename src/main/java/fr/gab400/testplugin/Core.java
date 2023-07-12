fecdf4c4a9a0041b6b4369f82bd693467b1df874
Gabriel <gabriel.cosme@orange.fr>
2023-07-12 15:15:13 +0200

    Added CommandManager and EventManager

--- src/main/java/fr/gab400/testplugin/Core.java
+++ src/main/java/fr/gab400/testplugin/Core.java
@@ -1,46 +1,27 @@
 package fr.gab400.testplugin;
 
-import fr.gab400.testplugin.commands.CommandHome;
-import fr.gab400.testplugin.commands.staff.*;
-import fr.gab400.testplugin.events.FreezeEvents;
-import fr.gab400.testplugin.events.JoinEvent;
-import fr.gab400.testplugin.events.LeaveEvent;
-import fr.gab400.testplugin.events.PlayersMenuEvents;
+import fr.gab400.testplugin.managers.CommandManager;
+import fr.gab400.testplugin.managers.EventManager;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Core extends JavaPlugin {
-
-    public static Core instance;
-
-    public static Core getInstance() {
-        return instance;
-    }
-
-    public ArrayList<Player> invisible_list = new ArrayList<>();
-
-    @Override
-    public void onEnable() {
-
-        instance = this;
-
-        // Commands
-        Objects.requireNonNull(getCommand("broadcast")).setExecutor(new CommandBroadcast(this));
-        Objects.requireNonNull(getCommand("gamemode")).setExecutor(new CommandGamemode());
-        Objects.requireNonNull(getCommand("home")).setExecutor(new CommandHome());
-        Objects.requireNonNull(getCommand("vanish")).setExecutor(new CommandVanish(this));
-        Objects.requireNonNull(getCommand("top")).setExecutor(new CommandTop());
-        Objects.requireNonNull(getCommand("players")).setExecutor(new CommandPlayers());
-        Objects.requireNonNull(getCommand("freeze")).setExecutor(new CommandFreeze());
-
-        // Events
-        getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
-        getServer().getPluginManager().registerEvents(new LeaveEvent(), this);
-        getServer().getPluginManager().registerEvents(new PlayersMenuEvents(), this);
-        getServer().getPluginManager().registerEvents(new FreezeEvents(), this);
-
-    }
+	
+	public static Core instance;
+	public ArrayList<Player> invisible_list = new ArrayList<>();
+	
+	public static Core getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public void onEnable() {
+		
+		instance = this;
+		
+		CommandManager commandManager = new CommandManager(this);
+		EventManager eventManager = new EventManager(this);
+	}
 }
